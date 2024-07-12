c2e6036b8c8a657687d01213ab3fcfb5ced29cfb
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-12 19:33:13 +0200

    [++]

--- frontend-service/src/container/index/Index.jsx
+++ frontend-service/src/container/index/Index.jsx
@@ -31,15 +31,16 @@ const Index = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className='bg-black h-full'>
+      <div className='bg-black h-[1000px]'>
+        <h1 className='text-white font-serif pt-40 pl-96 font-medium'>ANYTHING YOU WANNA DO PLEASE SHARE</h1>
         <input
           type='text'
           placeholder='What do you want to do?'
-          className='ml-10 mt-10 p-4 py-2 rounded-lg'
+          className='ml-96 mt-20 p-4 py-2 rounded-lg '
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
-        <button type='submit' className='text-black ml-4 border border-red-700 rounded-lg bg-red-700 p-2 py-px'>
+        <button type='submit' className=' text-center text-red-700  ml-4 border border-white rounded-lg bg-black p-2 py-px'>
           Add task
         </button>
       </div>
