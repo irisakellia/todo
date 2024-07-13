ff71674aab9cf1cabf8adb55c4269dcc190e1d05
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-13 13:16:57 +0200

    [adding ]

--- backend-service/controllers/taskcontroller.js
+++ backend-service/controllers/taskcontroller.js
@@ -116,27 +116,29 @@
 // module.exports = {addTask,getTask,getTasks,deleteTask,updateTask}
 
 
-const Task = require('../models/task.model')
+const Task = require('../models/task.model');
 
-const addTask = async(req,res)=>{
+const addTask = async (req, res) => {
+  try {
+    const { taskName } = req.body;
 
+    if (!taskName) {
+      return res.status(400).json({ success: false, message: "Task name is required" });
+    }
 
-try {
-    const {taskName} = req.body ;
-
-    const task = new Task({taskName});
+    const task = new Task({ taskName });
     await task.save();
-   console.log(task)
-   return res.status(200).json({success:true, message:"Task has been successfully added"})
-    
-} catch (error) {
-    res.status(500).json({success:false, message:"Internal server error"})
-}
 
+    console.log(task);
+    return res.status(200).json({ success: true, message: "Task has been successfully added" });
 
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
 
-}
+module.exports = { addTask };
 
-module.exports = {addTask}
 
 
--- frontend-service/src/container/index/Index.jsx
+++ frontend-service/src/container/index/Index.jsx
@@ -1,37 +1,34 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 
 const Index = () => {
-  
-  const [data, setData] = useState("")
+  const [data, setData] = useState({
+    task: '',
+  });
 
-  // const handleChange = (e)=>{
-  //   setValue({
-  //     ...value,
-  //     [e.target.name]: e.target.value,
-  //   })
-  // }
-  const handleSubmit = async (e) =>{
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-   try {
-    const response = await fetch("http://localhost:3000/add/task",{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json",
-      }
-     body:JSON.stringify(data),
+    try {
+      const response = await fetch("http://localhost:3000/add/task", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error("Failed to add a new task");
     }
-  
-  );
-
-  const result = await response.json();
-  console.log(result)
-
-    
-   } catch (error) {
-    throw new Error("failed to add a new task")
-   }
- 
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -39,13 +36,13 @@ const Index = () => {
         <h1 className='text-white font-serif pt-40 pl-96 font-medium'>ANYTHING YOU WANNA DO PLEASE SHARE</h1>
         <input
           type='text'
+          name='task'
           placeholder='What do you want to do?'
-          className='ml-96 mt-20 p-4 py-2 rounded-lg '
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
+          className='ml-96 mt-20 p-4 py-2 rounded-lg'
+          value={data.task}
+          onChange={handleChange}
         />
-        <button value={data.task}></button>
-        <button type='submit' className=' text-center text-red-700  ml-4 border border-white rounded-lg bg-black p-2 py-px'>
+        <button type='submit' className='text-center text-red-700 ml-4 border border-white rounded-lg bg-black p-2 py-px'>
           Add task
         </button>
       </div>
